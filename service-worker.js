7fda7ed1e7430e2e4e1812c320fe67446143ba66
Fortuitous <fortuitouspress@gmail.com>
2025-07-05 17:14:54 -0700

    Revise service worker

--- service-worker.js
+++ service-worker.js
@@ -20,7 +20,7 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  '/css/flashback.css' 
+  './css/flashback.css' 
   ];
 
 // The install handler takes care of precaching the resources we always need.
