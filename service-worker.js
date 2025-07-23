e4946b6277b52247669e63202fb7162a43bdbe98
Fortuitous <fortuitouspress@gmail.com>
2025-07-23 14:28:58 -0700

    Add console log of PRECACHE variable in SW

--- service-worker.js
+++ service-worker.js
@@ -15,6 +15,7 @@
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 const PRECACHE = 'precache-v9';
+console.log({PRECACHE});
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
